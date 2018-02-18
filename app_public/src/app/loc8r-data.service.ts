8f92bce12230598fc04226a3143cbcaf2935e3b8
Daniel <danwater1@gmail.com>
2018-02-17 23:51:13 -0800

    Moved app_server to single angular page

--- app_public/src/app/loc8r-data.service.ts
+++ app_public/src/app/loc8r-data.service.ts
@@ -10,9 +10,7 @@ export class Loc8rDataService {
 
   private apiBaseUrl = 'http://localhost:3000/api/';
 
-  public getLocations(): Promise<Location[]> {
-  	const lng: number = -0.969;
-  	const lat: number = 51.455;
+  public getLocations(lat: number, lng: number): Promise<Location[]> {
   	const maxDistance: number = 2000;
   	const url: string = `${this.apiBaseUrl}/locations?lng=${lng}&lat=${lat}&maxDistance=${maxDistance}`;
   	return this.http
